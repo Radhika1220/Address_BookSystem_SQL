10114677cf74be7639f6cb99d3c733431c92ea03
Radhika <radhika.shankar1220@gmail.com>
2021-07-30 22:40:30 +0530

    [radhika] insert into table

--- AddressBook.sql
+++ AddressBook.sql
@@ -4,3 +4,22 @@ create database AddressBookSystem
 
 use AddressBookSystem;
 
+-------------UC2------------------
+
+create table Address_Book_Table
+(
+FirstName varchar(50) not null,
+LastName varchar(20),
+Address varchar(100),
+City varchar(50),
+StateName varchar(55),
+ZipCode int,
+Phonenum bigint,
+EmailId varchar(100)
+);
+
+----------UC3-----------
+
+Insert into Address_Book_Table values('Logeswari','Jaisankar','Gandhi Bazaar','Bangalore','Karanataka',60048,8548712794,'logi12@gmail.com'),
+('Vijay','Kumar','Adyar Flyover','Chennai','TamilNadu',60015,987548512,'vijay123@yahoo.com');
+Insert into Address_Book_Table values('Sakthi','Rajan','Bharathi Dasan Nagar','Karaikal','TamilNadu',609602,9840781547,'sakthiraj@gmail.com');
